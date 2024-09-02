546d45562466440a6ec19bcddfc7a57ae9d158c9
Marc Piñol <github@marcpinol.com>
2024-09-02 17:24:50 +0200

    Added Eureka Server discoverability.

--- gateway-service/src/main/java/com/marcpinol/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/marcpinol/gatewayservice/GatewayServiceApplication.java
@@ -1,4 +1,4 @@
-package com.marcpinol.authservice;
+package com.marcpinol.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class AuthServiceApplication {
+public class GatewayServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AuthServiceApplication.class, args);
+        SpringApplication.run(GatewayServiceApplication.class, args);
     }
 
 }
